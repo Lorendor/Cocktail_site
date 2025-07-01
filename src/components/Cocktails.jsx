843f1417828681e528cb5877d63cb1a51f41de6a
Lorendor <97479904+Lorendor@users.noreply.github.com>
2025-07-02 01:03:34 +0200

    implement Cocktails section

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { cocktailLists, mockTailLists } from '../../constants/index.js'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+const Cocktails = () => {
+
+  useGSAP(() => {
+   const parallaxTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#cocktails',
+      start: 'top 30%',
+      end: 'bottom 80%',
+      scrub: true,
+    }
+    })
+    parallaxTimeline
+      .from('#c-left-leaf', {
+        x: -100,
+        y: 100,
+  })
+      .from('#c-right-leaf', {
+        x: 100,
+        y: 100,
+      })
+  })
+
+  return (
+    <section id='cocktails'>
+      <img src="/images/cocktail-left-leaf.png" alt='left-leaf' id='c-left-leaf'></img>
+      <img src="/images/cocktail-right-leaf.png" alt='right-leaf' id='c-right-leaf'></img>
+
+      <div className='list'>
+        <div className='popular'>
+          <h2>Popular Cocktails</h2>
+
+          <ul>
+            {cocktailLists.map(({name,country,detail,price}) => (
+              <li key={name}>
+                <div className='md:me-28'>
+                  <h3>{name}</h3>
+                  <p>{country} | {detail}</p>
+                </div>
+                <span>{price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='popular'>
+          <h2>Virgin Cocktails</h2>
+          
+          <ul>
+            {mockTailLists.map(({name,country,detail,price}) => (
+              <li key={name}>
+                <div className='md:me-28'>
+                  <h3>{name}</h3>
+                  <p>{country} | {detail}</p>
+                </div>
+                <span>{price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Cocktails
